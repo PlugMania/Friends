6777c2223702df5ef6a7722c23b6f61a51392b15
korikisulda <Korikisulda@plugmania.info>
2012-07-07 17:43:33 +0100

    added friends and friendmanager classes and methods

---
+++ src/info/plugmania/friends/Friend.java
@@ -0,0 +1,14 @@
+package info.plugmania.friends;
+
+import java.util.ArrayList;
+
+public class Friend {
+public ArrayList<Friend> getFriends(){
+	return null; //todo: add something here
+}
+
+public ArrayList<Friend> getPendingFriendRequests(){
+	return null; //todo: add something here
+}
+
+}
---
+++ src/info/plugmania/friends/FriendManager.java
@@ -0,0 +1,13 @@
+package info.plugmania.friends;
+
+import org.bukkit.entity.Player;
+
+public class FriendManager {
+Friends plugin;
+public FriendManager(Friends instance){
+	plugin=instance;
+}
+public Friend getFriend(Player p){
+	return null; //todo
+}
+}
