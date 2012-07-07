dc6c41758fc15747d2f42ab450052206dc347651
korikisulda <Korikisulda@plugmania.info>
2012-07-07 17:48:10 +0100

    added getName() to friend

--- src/info/plugmania/friends/Friend.java
+++ src/info/plugmania/friends/Friend.java
@@ -3,12 +3,18 @@ package info.plugmania.friends;
 import java.util.ArrayList;
 
 public class Friend {
+	
+	public String name;
 public ArrayList<Friend> getFriends(){
 	return null; //todo: add something here
 }
 
 public ArrayList<Friend> getPendingFriendRequests(){
 	return null; //todo: add something here
+}
+
+public String getName(){
+	return name;
 }
 
 }
